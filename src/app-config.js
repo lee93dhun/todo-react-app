17b8211d40f6ab645b14ff3b0e051c28176bbabd
lee93 <dohuny2@naver.com>
2023-06-26 21:14:51 +0900

    2023-06-26

---
+++ src/app-config.js
@@ -0,0 +1,12 @@
+let backendHost;
+
+const hostname = window && window.location && window.location.hostname;
+
+if(hostname === "localhost"){
+    backendHost = "http://localhost:8080";
+}
+
+// const API_BASE_URL = `${backendHost}`;
+// export default API_BASE_URL;
+
+export const API_BASE_URL = `${backendHost}`;
